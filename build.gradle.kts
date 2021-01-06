3895f3c2ef26d3052e84e4e738b623997a7e6c6d
Ali Albaali <ali@albaali.com>
2021-01-06 13:53:12 +0300

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.20"
+    id("org.jetbrains.compose") version "0.3.0-build135"
+    `maven-publish`
+}
+
+group = "io.kamel"
+version = "0.0.1"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+    maven { url = uri("https://kotlin.bintray.com/ktor") }
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    val ktor_version = "1.5.0"
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-apache:$ktor_version")
+}
+
+kotlin {
+    explicitApi = ExplicitApiMode.Warning
+    target {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "11"
+                freeCompilerArgs = listOf("-Xallow-result-return-type", "-Xopt-in=kotlin.RequiresOptIn")
+            }
+        }
+    }
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "io.kamel"
+            artifactId = "core"
+            version = "0.1.0"
+
+            from(components["kotlin"])
+        }
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Kamel"
+        }
+    }
+}
