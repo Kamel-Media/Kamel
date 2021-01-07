093d42fd8f22cb0c027ede493ff85f0bf068530d
Ali Albaali <ali@albaali.com>
2021-01-07 21:39:25 +0300

    Add lazyImageResource for File

--- src/main/kotlin/io/kamel/core/LazyImageResource.kt
+++ src/main/kotlin/io/kamel/core/LazyImageResource.kt
@@ -2,65 +2,54 @@ package io.kamel.core
 
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
 import io.kamel.core.fetcher.FileFetcher
 import io.kamel.core.fetcher.HttpFetcher
 import io.kamel.core.utils.asResource
 import io.kamel.core.utils.toUrl
-import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import java.io.File
-import java.net.URI
-import java.net.URL
 
 internal val EmptyRequestBuilder: HttpRequestBuilder.() -> Unit get() = {}
 
-/**
- * in terms of resource loading
- * options are either return an [Resource] or callback that contains an [Resource]
- * in terms of Image functions such as [LazyImage] or [LazyImage] these might work
- * */
 @Composable
 public fun lazyImageResource(
-    url: String,
-    requestBuilder: HttpRequestBuilder.() -> Unit = EmptyRequestBuilder
-): Resource<ImageBitmap> = lazyImageResource(url.toUrl(), requestBuilder)
-
-@Composable
-public fun lazyImageResource(
-    url: Url,
-    requestBuilder: HttpRequestBuilder.() -> Unit = EmptyRequestBuilder
+    data: File,
 ): Resource<ImageBitmap> {
 
-    var resource by remember(url) { mutableStateOf<Resource<ImageBitmap>>(Resource.Loading) }
+    var resource by remember(data) { mutableStateOf<Resource<ImageBitmap>>(Resource.Loading) }
 
-    val client = AmbientHttpClient.current
-
-    LaunchedEffect(url, client, requestBuilder) {
-        resource = fetchData(url, client, requestBuilder)
-            ?.asResource() ?: Resource.Failure(Throwable("Something went wrong"))
+    LaunchedEffect(data) {
+        resource = FileFetcher.fetch(data)
+            .mapCatching { it.asResource() }
+            .getOrElse { Resource.Failure(it) }
     }
 
     return resource
 }
 
-internal suspend inline fun <T : Any> fetchData(
-    data: T,
-    client: HttpClient,
-    requestBuilder: HttpRequestBuilder.() -> Unit
-) = runCatching {
-    when (data) {
-        is File -> FileFetcher.fetch(data)
-        is String -> HttpFetcher(client).fetch(data.toUrl(), requestBuilder)
-        is Url -> HttpFetcher(client).fetch(data, requestBuilder)
-        is URL -> HttpFetcher(client).fetch(data.toUrl(), requestBuilder)
-        is URI -> HttpFetcher(client).fetch(data.toUrl(), requestBuilder)
-        is ImageVector -> throw IllegalArgumentException("Unsupported type: ImageVector, use androidx.compose.foundation.Image()")
-        is Painter -> throw IllegalArgumentException("Unsupported type: Painter, use androidx.compose.foundation.Image()")
-        is ImageBitmap -> throw IllegalArgumentException("Unsupported type: ImageBitmap, use androidx.compose.foundation.Image()")
-        else -> throw IllegalArgumentException("Unsupported type")
-    }
-}.getOrNull()?.getOrNull()
+@Composable
+public fun lazyImageResource(
+    data: String,
+    requestBuilder: HttpRequestBuilder.() -> Unit = EmptyRequestBuilder
+): Resource<ImageBitmap> = lazyImageResource(data.toUrl(), requestBuilder)
 
+@Composable
+public fun lazyImageResource(
+    data: Url,
+    requestBuilder: HttpRequestBuilder.() -> Unit = EmptyRequestBuilder
+): Resource<ImageBitmap> {
+
+    var resource by remember(data) { mutableStateOf<Resource<ImageBitmap>>(Resource.Loading) }
+
+    val client = AmbientHttpClient.current
+
+    LaunchedEffect(data, client, requestBuilder) {
+        resource = HttpFetcher(client, requestBuilder)
+            .fetch(data)
+            .mapCatching { it.asResource() }
+            .getOrElse { Resource.Failure(it) }
+    }
+
+    return resource
+}
