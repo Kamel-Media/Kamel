336c8962f59146b61753d34b4ca65c39c12055d3
Ali Albaali <ali@albaali.com>
2021-01-14 01:49:51 +0300

    Bump Compose to build140

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
-    id("org.jetbrains.compose") version "0.3.0-build139"
+    id("org.jetbrains.compose") version "0.3.0-build140"
     `maven-publish`
 }
 
