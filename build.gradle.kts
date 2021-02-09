59ac77741841d663698f33feefe19f84db87748a
Ali Albaali <ali@albaali.com>
2021-02-09 04:05:32 +0300

    bump version to 0.0.7-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
+
 plugins {
     id("org.jetbrains.dokka") version "1.4.20"
 }
@@ -19,7 +22,22 @@ buildscript {
     }
 }
 
+Properties().apply {
+    load(rootProject.file("local.properties").inputStream())
+    ext {
+        set("signing.keyId", getProperty("signing.keyId"))
+        set("signing.password", getProperty("signing.password"))
+        set("ossrh.username", getProperty("ossrh.username"))
+        set("ossrh.password", getProperty("ossrh.password"))
+        set("signing.secretKeyRingFile", getProperty("signing.secretKeyRingFile"))
+    }
+}
+
 allprojects {
+
+    group = "com.alialbaali.kamel"
+    version = "0.0.7-SNAPSHOT"
+
     repositories {
         google()
         jcenter()
@@ -29,11 +47,73 @@ allprojects {
         maven(url = "https://kotlin.bintray.com/ktor")
     }
 
-    ext {
-        set("GroupId", "com.alialbaali.kamel")
-        set("Version", "0.0.6")
+    val emptyJavadocJar by tasks.registering(Jar::class) {
+        archiveClassifier.set("javadoc")
     }
 
-    group = ext["GroupId"] as String
-    version = ext["Version"] as String
+    afterEvaluate {
+        extensions.findByType<PublishingExtension>()?.apply {
+            publications.withType<MavenPublication>().configureEach {
+
+                artifact(emptyJavadocJar.get())
+
+                pom {
+
+                    name.set("Kamel")
+                    description.set("Kotlin Asynchronous Media Loading Library that supports Compose")
+                    url.set("https://github.com/alialbaali/Kamel")
+
+                    licenses {
+                        license {
+                            name.set("The Apache Software License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                            distribution.set("repo")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("alialbaali")
+                            name.set("Ali Albaali")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:github.com/alialbaali/Kamel.git")
+                        developerConnection.set("scm:git:ssh://github.com/alialbaali/Kamel.git")
+                        url.set("https://github.com/alialbaali/Kamel/tree/main")
+                    }
+
+                }
+
+            }
+
+            repositories {
+                maven {
+
+                    name = "MavenCentral"
+
+                    val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                    val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
+
+                    url = if (version.toString().endsWith("SNAPSHOT")) uri(snapshotsRepoUrl) else uri(releasesRepoUrl)
+
+                    credentials {
+                        username = rootProject.ext["ossrh.username"] as String
+                        password = rootProject.ext["ossrh.password"] as String
+                    }
+
+                }
+            }
+
+            extensions.findByType<SigningExtension>()?.sign(publications)
+        }
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xallow-result-return-type", "-Xopt-in=kotlin.RequiresOptIn")
+        }
+    }
+
 }
