601f9dcc397b26d3ffbe8a2965bdee6060c05222
Ali Albaali <ali@albaali.com>
2021-02-21 19:56:27 +0300

    Update FileFetcher

--- kamel-core/src/jvmMain/kotlin/io/kamel/core/fetcher/JvmFileFetcher.kt
+++ kamel-core/src/jvmMain/kotlin/io/kamel/core/fetcher/JvmFileFetcher.kt
@@ -1,10 +1,7 @@
 package io.kamel.core.fetcher
 
 import io.kamel.core.DataSource
-import io.kamel.core.ExperimentalKamelApi
-import io.kamel.core.Resource
 import io.kamel.core.config.ResourceConfig
-import io.kamel.core.utils.tryCatching
 import io.ktor.util.cio.*
 import io.ktor.utils.io.*
 import java.io.File
@@ -19,8 +16,8 @@ internal actual object FileFetcher : Fetcher<File> {
     override val File.isSupported: Boolean
         get() = exists() && isFile
 
-    override suspend fun fetch(data: File, resourceConfig: ResourceConfig): Result<ByteReadChannel> = runCatching {
-        data.readChannel()
+    override suspend fun fetch(data: File, resourceConfig: ResourceConfig): ByteReadChannel {
+        return data.readChannel()
     }
 
 }
