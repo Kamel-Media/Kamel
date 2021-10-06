73b9be7b3249a49d617a394ec7996add32d5ebff
Ali Albaali <ali@albaali.com>
2021-10-06 22:03:07 +0300

    Update ImageBitmapDecoder to use Skia Image

--- kamel-image/src/desktopMain/kotlin/io/kamel/image/decoder/DesktopImageBitmapDecoder.kt
+++ kamel-image/src/desktopMain/kotlin/io/kamel/image/decoder/DesktopImageBitmapDecoder.kt
@@ -6,16 +6,16 @@ import io.kamel.core.config.ResourceConfig
 import io.kamel.core.decoder.Decoder
 import io.ktor.util.*
 import io.ktor.utils.io.*
-import org.jetbrains.skija.Image
+import org.jetbrains.skia.Image
 
 /**
- * Decodes and transfers [ByteReadChannel] to [ImageBitmap] using Skija [Image].
+ * Decodes and transfers [ByteReadChannel] to [ImageBitmap] using Skia [Image].
  */
 internal actual object ImageBitmapDecoder : Decoder<ImageBitmap> {
 
-    override suspend fun decode(channel: ByteReadChannel, resourceConfig: ResourceConfig): ImageBitmap {
-        return Image.makeFromEncoded(channel.toByteArray())
-            .asImageBitmap()
-    }
+    override suspend fun decode(
+        channel: ByteReadChannel,
+        resourceConfig: ResourceConfig
+    ): ImageBitmap = Image.makeFromEncoded(channel.toByteArray()).asImageBitmap()
 
 }
