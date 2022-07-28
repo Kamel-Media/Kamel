eaf6e1c80543db9568b9374ce974cb8626c49c05
Ali Albaali <ali@albaali.com>
2022-07-28 17:22:57 +0300

    Refactor ExperimentalKamelApi message

--- kamel-core/src/commonMain/kotlin/io/kamel/core/ExperimentalKamelApi.kt
+++ kamel-core/src/commonMain/kotlin/io/kamel/core/ExperimentalKamelApi.kt
@@ -1,5 +1,8 @@
 package io.kamel.core
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
-@RequiresOptIn(message = "This is an experimental Kamal API. It will likely be removed or changed in the future", level = RequiresOptIn.Level.WARNING)
+@RequiresOptIn(
+    message = "This is an experimental Kamel API, and it is likely to be changed or removed in the future.",
+    level = RequiresOptIn.Level.WARNING
+)
 public annotation class ExperimentalKamelApi
