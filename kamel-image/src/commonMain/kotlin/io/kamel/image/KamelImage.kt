cd1df5a5fbaf36c81eb63fd9e2307d3f03d655d1
Ali Albaali <ali@albaali.com>
2022-07-28 20:05:40 +0300

    Refactor KamelImage composable & Introduce new experimental composable KamelImageBox

--- kamel-image/src/commonMain/kotlin/io/kamel/image/KamelImage.kt
+++ kamel-image/src/commonMain/kotlin/io/kamel/image/KamelImage.kt
@@ -2,25 +2,32 @@ package io.kamel.image
 
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.FiniteAnimationSpec
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import io.kamel.core.ExperimentalKamelApi
 import io.kamel.core.Resource
 
 /**
- * A composable that displays an image resource.
- * To load an image resource use [lazyImageResource].
- * @param onLoading Composable that can be used while loading the image.
- * @param onFailure Composable that can be used when the image result is failure.
- * @param crossfade whether [Crossfade] is enabled or not.
- * @param animationSpec a [FiniteAnimationSpec] to be used in [crossfade] animation.
+ * A composable that is used to display a [Painter] resource.
+ * To load an image resource asynchronously, use [lazyPainterResource].
+ * @param resource The [Resource] that needs to be displayed.
+ * @param modifier The modifier that is applied to the [Box].
+ * @param onLoading Composable which is used while the image is in [Resource.Loading] state.
+ * @param onFailure Composable which is used while the image is in [Resource.Failure] state.
+ * @param contentAlignment The default alignment inside the Box.
+ * @param animationSpec a [FiniteAnimationSpec] to be used in [Crossfade] animation, or null to be disabled.
+ * The rest is the default [Image] parameters.
  */
+@OptIn(ExperimentalKamelApi::class)
 @Composable
 public fun KamelImage(
     resource: Resource<Painter>,
@@ -30,63 +37,70 @@ public fun KamelImage(
     contentScale: ContentScale = ContentScale.Fit,
     alpha: Float = DefaultAlpha,
     colorFilter: ColorFilter? = null,
-    onLoading: ((Float) -> Unit)? = null,
-    onFailure: ((Throwable) -> Unit)? = null,
-    crossfade: Boolean = false,
-    animationSpec: FiniteAnimationSpec<Float> = tween()
+    onLoading: @Composable (BoxScope.(Float) -> Unit)? = null,
+    onFailure: @Composable (BoxScope.(Throwable) -> Unit)? = null,
+    contentAlignment: Alignment = Alignment.Center,
+    animationSpec: FiniteAnimationSpec<Float>? = null,
 ) {
-    if (crossfade)
-        Crossfade(resource, animationSpec = animationSpec) { animatedResource ->
-            DefaultImage(
-                animatedResource,
-                contentDescription,
-                modifier,
-                alignment,
-                contentScale,
-                alpha,
-                colorFilter,
-                onLoading,
-                onFailure,
-            )
-        }
-    else
-        DefaultImage(
-            resource,
+    val onSuccess: @Composable (BoxScope.(Painter) -> Unit) = { painter ->
+        Image(
+            painter,
             contentDescription,
-            modifier,
+            Modifier.fillMaxSize(),
             alignment,
             contentScale,
             alpha,
-            colorFilter,
-            onLoading,
-            onFailure,
+            colorFilter
         )
+    }
+    KamelImageBox(
+        resource,
+        modifier,
+        contentAlignment,
+        animationSpec,
+        onLoading,
+        onFailure,
+        onSuccess,
+    )
 }
 
+/**
+ * A composable that is used to display a [Painter] resource.
+ * To load an image [Resource] asynchronously, use [lazyPainterResource].
+ * @param resource The [Resource] that needs to be displayed.
+ * @param modifier The modifier that is applied to the [Box].
+ * @param contentAlignment The default alignment inside the Box.
+ * @param animationSpec a [FiniteAnimationSpec] to be used in [Crossfade] animation, or null to be disabled.
+ * @param onLoading Composable which is used while the image is in [Resource.Loading] state.
+ * @param onFailure Composable which is used while the image is in [Resource.Failure] state.
+ * @param onSuccess Composable which is used while the image is in [Resource.Success] state.
+ */
+@ExperimentalKamelApi
 @Composable
-private fun DefaultImage(
+public fun KamelImageBox(
     resource: Resource<Painter>,
-    contentDescription: String?,
     modifier: Modifier = Modifier,
-    alignment: Alignment = Alignment.Center,
-    contentScale: ContentScale = ContentScale.Fit,
-    alpha: Float = DefaultAlpha,
-    colorFilter: ColorFilter? = null,
-    onLoading: ((Float) -> Unit)? = null,
-    onFailure: ((Throwable) -> Unit)? = null,
+    contentAlignment: Alignment = Alignment.Center,
+    animationSpec: FiniteAnimationSpec<Float>? = null,
+    onLoading: @Composable (BoxScope.(Float) -> Unit)? = null,
+    onFailure: @Composable (BoxScope.(Throwable) -> Unit)? = null,
+    onSuccess: @Composable BoxScope.(Painter) -> Unit,
 ) {
-    when (resource) {
-        is Resource.Loading -> if (onLoading != null) onLoading(resource.progress)
-        is Resource.Success ->
-            Image(
-                resource.value,
-                contentDescription,
-                modifier,
-                alignment,
-                contentScale,
-                alpha,
-                colorFilter
-            )
-        is Resource.Failure -> if (onFailure != null) onFailure(resource.exception)
+    Box(modifier, contentAlignment) {
+        if (animationSpec != null) {
+            Crossfade(resource, animationSpec = animationSpec) { animatedResource ->
+                when (animatedResource) {
+                    is Resource.Loading -> if (onLoading != null) onLoading(animatedResource.progress)
+                    is Resource.Success -> onSuccess(animatedResource.value)
+                    is Resource.Failure -> if (onFailure != null) onFailure(animatedResource.exception)
+                }
+            }
+        } else {
+            when (resource) {
+                is Resource.Loading -> if (onLoading != null) onLoading(resource.progress)
+                is Resource.Success -> onSuccess(resource.value)
+                is Resource.Failure -> if (onFailure != null) onFailure(resource.exception)
+            }
+        }
     }
 }
