6242db20c99c70b7c4bb3521c3a302aea1147896
Ali Albaali <ali@albaali.com>
2022-08-01 23:16:19 +0300

    Update ResourcesFetcher test

--- kamel-image/src/desktopTest/kotlin/io/kamel/image/fetcher/ResourcesFetcherTest.kt
+++ kamel-image/src/desktopTest/kotlin/io/kamel/image/fetcher/ResourcesFetcherTest.kt
@@ -1,35 +1,60 @@
 package io.kamel.image.fetcher
 
-import androidx.compose.ui.unit.Density
+import io.kamel.core.DataSource
 import io.kamel.core.config.ResourceConfigBuilder
+import io.kamel.core.getOrNull
+import io.kamel.core.isLoading
+import io.kamel.core.map
 import io.ktor.http.*
 import io.ktor.util.*
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class ResourcesFetcherTest {
 
     private val fetcher = ResourcesFetcher
-    private val resourceConfig = ResourceConfigBuilder()
-        .apply { density = Density(1F) }
-        .build()
+    private val resourceConfig = ResourceConfigBuilder().build()
 
     @Test
-    fun loadImageBitmapResource(): Unit = runBlockingTest {
+    fun testUrlIsSupported(): Unit = runTest {
         val imageUrl = Url("Compose.png")
-        val bytes = fetcher.fetch(imageUrl, resourceConfig).toByteArray()
+        val isSupported = with(fetcher) { imageUrl.isSupported }
 
-        assertTrue { bytes.isNotEmpty() }
+        assertTrue { isSupported }
     }
 
     @Test
-    fun loadInvalidImageResource(): Unit = runBlockingTest {
+    fun testUrlIsNotSupported(): Unit = runTest {
+        val imageUrl = Url("invalidImage.jpg")
+        val isSupported = with(fetcher) { imageUrl.isSupported }
+
+        assertFalse { isSupported }
+    }
+
+    @Test
+    fun loadImageBitmapResource(): Unit = runTest {
+        val imageUrl = Url("Compose.png")
+        val resource = fetcher.fetch(imageUrl, resourceConfig)
+            .first { !it.isLoading }
+            .map { it.toByteArray() }
+
+        assertTrue { resource.getOrNull()!!.isNotEmpty() }
+        assertTrue { resource.source == DataSource.Disk }
+    }
+
+    @Test
+    fun loadInvalidImageResource(): Unit = runTest {
         val imageUrl = Url("invalidImage.jpg")
 
         assertFailsWith<IllegalStateException> {
             fetcher.fetch(imageUrl, resourceConfig)
+                .first { !it.isLoading }
         }
     }
 
