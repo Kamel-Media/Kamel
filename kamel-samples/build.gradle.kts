1e6774a9726583f2dc6e5f98ea38ce4b9eeed826
Luca Spinazzola <mspinluca@gmail.com>
2022-08-10 19:02:47 -0400

    fix adding  moko resources to native app bundle in native desktop sample

--- kamel-samples/build.gradle.kts
+++ kamel-samples/build.gradle.kts
@@ -1,4 +1,11 @@
+import org.jetbrains.compose.desktop.application.tasks.AbstractNativeMacApplicationPackageAppDirTask
 import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractExecutable
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBinary
+import org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink
+import org.jetbrains.kotlin.library.impl.KotlinLibraryLayoutImpl
+import org.jetbrains.kotlin.konan.file.File as KonanFile
+import java.io.FileFilter
 
 plugins {
     multiplatform
@@ -94,6 +101,7 @@ kotlin {
         }
 
         val desktopMain by getting {
+            dependsOn(commonMain)
             dependencies {
                 implementation(compose.desktop.currentOs)
                 implementation(Dependencies.Ktor.CIO)
@@ -140,5 +148,68 @@ compose.desktop.nativeApplication {
         targetFormats(org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg)
         packageName = "Native-Sample"
         packageVersion = "1.0.0"
+    }
+}
+
+
+// todo: Remove when resolved: https://github.com/icerockdev/moko-resources/issues/372
+tasks.withType<KotlinNativeLink>()
+    .matching { linkTask -> linkTask.binary is AbstractExecutable }
+    .configureEach {
+        val task: KotlinNativeLink = this
+
+        doLast {
+            val binary: NativeBinary = task.binary
+            val outputDir: File = task.outputFile.get().parentFile
+            task.libraries
+                .filter { library -> library.extension == "klib" }
+                .filter(File::exists)
+                .forEach { inputFile ->
+                    val klibKonan = KonanFile(inputFile.path)
+                    val klib = KotlinLibraryLayoutImpl(
+                        klib = klibKonan,
+                        component = "default"
+                    )
+                    val layout = klib.extractingToTemp
+
+                    // extracting bundles
+                    layout
+                        .resourcesDir
+                        .absolutePath
+                        .let(::File)
+                        .listFiles(FileFilter { it.extension == "bundle" })
+                        // copying bundles to app
+                        ?.forEach { bundleFile ->
+                            logger.info("${bundleFile.absolutePath} copying to $outputDir")
+                            bundleFile.copyRecursively(
+                                target = File(outputDir, bundleFile.name),
+                                overwrite = true
+                            )
+                        }
+                }
+        }
+    }
+
+tasks.withType<AbstractNativeMacApplicationPackageAppDirTask> {
+    val task: AbstractNativeMacApplicationPackageAppDirTask = this
+
+    doLast {
+        val execFile: File = task.executable.get().asFile
+        val execDir: File = execFile.parentFile
+        val destDir: File = task.destinationDir.asFile.get()
+        val bundleID: String = task.bundleID.get()
+
+        val outputDir = File(destDir, "$bundleID.app/Contents/Resources")
+        outputDir.mkdirs()
+
+        execDir.listFiles().orEmpty()
+            .filter { it.extension == "bundle" }
+            .forEach { bundleFile ->
+                logger.info("${bundleFile.absolutePath} copying to $outputDir")
+                bundleFile.copyRecursively(
+                    target = File(outputDir, bundleFile.name),
+                    overwrite = true
+                )
+            }
     }
 }
