f97d9c87e55f6ca49f80d592e0e0595dd963fdb5
Luca Spinazzola <mspinluca@gmail.com>
2023-05-06 18:30:30 -0400

    fix native tests

--- kamel-core/build.gradle.kts
+++ kamel-core/build.gradle.kts
@@ -1,6 +1,11 @@
-import org.jetbrains.compose.compose
 import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractExecutable
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBinary
+import org.jetbrains.kotlin.gradle.plugin.mpp.TestExecutable
+import org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink
+import org.jetbrains.kotlin.library.impl.KotlinLibraryLayoutImpl
+import java.io.FileFilter
 
 plugins {
     multiplatform
@@ -123,3 +128,44 @@ tasks.withType<AbstractPublishToMaven>().configureEach {
     dependsOnTasks.add(this.name.replace("publish", "sign").replaceAfter("Publication", ""))
     dependsOn(dependsOnTasks)
 }
+
+
+// todo: Remove when resolved: https://github.com/icerockdev/moko-resources/issues/372
+tasks.withType<KotlinNativeLink>()
+    .matching { linkTask ->
+        linkTask.binary is AbstractExecutable
+    }
+    .configureEach {
+        val task: KotlinNativeLink = this
+
+        this.doLast {
+            val binary: NativeBinary = task.binary
+            val outputDir: File = task.outputFile.get().parentFile
+            task.libraries
+                .filter { library -> library.extension == "klib" }
+                .filter(File::exists)
+                .forEach { inputFile ->
+                    val klibKonan = org.jetbrains.kotlin.konan.file.File(inputFile.path)
+                    val klib = KotlinLibraryLayoutImpl(
+                        klib = klibKonan,
+                        component = "default"
+                    )
+                    val layout = klib.extractingToTemp
+
+                    // extracting bundles
+                    layout
+                        .resourcesDir
+                        .absolutePath
+                        .let(::File)
+                        .listFiles(FileFilter { it.extension == "bundle" })
+                        // copying bundles to app
+                        ?.forEach { bundleFile ->
+                            logger.info("${bundleFile.absolutePath} copying to $outputDir")
+                            bundleFile.copyRecursively(
+                                target = File(outputDir, bundleFile.name),
+                                overwrite = true
+                            )
+                        }
+                }
+        }
+    }
