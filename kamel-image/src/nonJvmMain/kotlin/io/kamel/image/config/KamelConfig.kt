297f75a018021cf8f613e2c36e6f8bbcab610e31
Luca Spinazzola <mspinluca@gmail.com>
2023-05-07 17:46:48 -0400

    remove resourcesFetcher from nonJvm. It's not really applicable

--- kamel-image/src/nonJvmMain/kotlin/io/kamel/image/config/KamelConfig.kt
+++ kamel-image/src/nonJvmMain/kotlin/io/kamel/image/config/KamelConfig.kt
@@ -3,12 +3,6 @@ package io.kamel.image.config
 import io.kamel.core.config.KamelConfigBuilder
 import io.kamel.image.decoder.ImageVectorDecoder
 import io.kamel.image.decoder.SvgDecoder
-import io.kamel.image.fetcher.ResourcesFetcher
-
-/**
- * Adds application resources fetcher to the [KamelConfigBuilder].
- */
-public fun KamelConfigBuilder.resourcesFetcher(): Unit = fetcher(ResourcesFetcher)
 
 public fun KamelConfigBuilder.imageVectorDecoder(): Unit = decoder(ImageVectorDecoder)
 
