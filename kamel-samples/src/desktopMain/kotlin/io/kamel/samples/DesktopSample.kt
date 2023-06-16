c25739be97edeedb7f835002396b3a6888ddfeb0
Luca Spinazzola <mspinluca@gmail.com>
2023-06-16 00:23:14 -0400

    add desktop preview to sample

--- kamel-samples/src/desktopMain/kotlin/io/kamel/samples/DesktopSample.kt
+++ kamel-samples/src/desktopMain/kotlin/io/kamel/samples/DesktopSample.kt
@@ -1,14 +1,32 @@
 package io.kamel.samples
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.singleWindowApplication
 import io.kamel.core.config.KamelConfig
 import io.kamel.core.config.takeFrom
 import io.kamel.image.config.*
+import io.kamel.tests.MR
 
 public actual val cellsCount: Int = 4
 
 public fun main(): Unit = singleWindowApplication {
+    val kamelConfig = remember {
+        KamelConfig {
+            takeFrom(KamelConfig.Default)
+            resourcesFetcher()
+            imageVectorDecoder()
+            svgDecoder()
+            imageBitmapDecoder()
+        }
+    }
+    launcher(kamelConfig)
+}
+
+@Preview
+@Composable
+public fun desktopSample() {
     val kamelConfig = remember {
         KamelConfig {
             takeFrom(KamelConfig.Default)
