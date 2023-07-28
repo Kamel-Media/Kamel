02bf0934a6e46dd367311ef2a6db41449a3c3d62
Luca Spinazzola <mspinluca@gmail.com>
2023-07-27 20:05:19 -0400

    add back publish signing workaround

--- kamel-core/build.gradle.kts
+++ kamel-core/build.gradle.kts
@@ -167,4 +167,11 @@ tasks.register<Copy>("jsCopyResourcesFromKamelTests") {
 }
 tasks.getByName("jsProcessResources").dependsOn("jsCopyResourcesFromKamelTests")
 
+// https://youtrack.jetbrains.com/issue/KT-46466
+val dependsOnTasks = mutableListOf<String>()
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    dependsOnTasks.add(this.name.replace("publish", "sign").replaceAfter("Publication", ""))
+    dependsOn(dependsOnTasks)
+}
+
 apply(from = "$rootDir/gradle/pack-core-tests-resources.gradle.kts")
