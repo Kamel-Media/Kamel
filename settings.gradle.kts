b366e429afeda12cb3ccdfad14459149c64028b0
Luca Spinazzola <mspinluca@gmail.com>
2023-08-19 14:51:00 -0400

    update refreshVersions and gradlew

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
         google()
     }
     plugins {
-        id("de.fayard.refreshVersions") version "0.51.0"
+        id("de.fayard.refreshVersions") version "0.60.0"
     }
 }
 
