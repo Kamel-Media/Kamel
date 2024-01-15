9b3dcf2e9cb2c0d348bf2d59f6559f2031c88858
Luca Spinazzola <mspinluca@gmail.com>
2024-01-15 15:41:33 +0100

    fix #65

--- kamel-samples/src/commonMain/kotlin/io/kamel/samples/FileSample.kt
+++ kamel-samples/src/commonMain/kotlin/io/kamel/samples/FileSample.kt
@@ -4,16 +4,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import io.kamel.core.config.KamelConfig
 import io.kamel.core.utils.File
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
-import io.kamel.image.config.LocalKamelConfig
 import kotlinx.coroutines.launch
 
 
 @Composable
-internal fun FileSample(fileResourcePath: String, kamelConfig: KamelConfig, context: Any? = null) {
+internal fun FileSample(fileResourcePath: String) {
 
     val scope = rememberCoroutineScope()
     var file: File? by remember { mutableStateOf(null) }
@@ -22,16 +20,13 @@ internal fun FileSample(fileResourcePath: String, kamelConfig: KamelConfig, cont
         file = getResourceFile(fileResourcePath)
     }
 
-
-    CompositionLocalProvider(LocalKamelConfig provides kamelConfig) {
-        Column {
-            file?.let { file ->
-                val painterResource = asyncPainterResource(file)
-                KamelImage(painterResource,
-                    contentDescription = "Compose",
-                    modifier = Modifier.fillMaxSize(),
-                    onFailure = { throw it })
-            }
+    Column {
+        file?.let { file ->
+            val painterResource = asyncPainterResource(file)
+            KamelImage(painterResource,
+                contentDescription = "Compose",
+                modifier = Modifier.fillMaxSize(),
+                onFailure = { throw it })
         }
     }
 }
--- kamel-samples/src/commonMain/kotlin/io/kamel/samples/launcher.kt
+++ kamel-samples/src/commonMain/kotlin/io/kamel/samples/launcher.kt
@@ -8,42 +8,41 @@ import androidx.compose.runtime.*
 import io.kamel.core.config.KamelConfig
 import io.kamel.image.config.LocalKamelConfig
 
-@androidx.compose.runtime.Composable
-public fun launcher(kamelConfig: KamelConfig, context: Any? = null) {
-    var sampleIndex by remember { mutableStateOf(0) }
-    Column {
-        Row {
-            Button({
-                sampleIndex = 0
-            }) {
-                Text("Gallery")
+@Composable
+public fun launcher(kamelConfig: KamelConfig) {
+    CompositionLocalProvider(LocalKamelConfig provides kamelConfig) {
+        var sampleIndex by remember { mutableStateOf(0) }
+        Column {
+            Row {
+                Button({
+                    sampleIndex = 0
+                }) {
+                    Text("Gallery")
+                }
+                Button({
+                    sampleIndex = 1
+                }) {
+                    Text("Bitmap")
+                }
+                Button({
+                    sampleIndex = 2
+                }) {
+                    Text("Svg")
+                }
+                Button({
+                    sampleIndex = 3
+                }) {
+                    Text("Xml")
+                }
             }
-            Button({
-                sampleIndex = 1
-            }) {
-                Text("Bitmap")
-            }
-            Button({
-                sampleIndex = 2
-            }) {
-                Text("Svg")
-            }
-            Button({
-                sampleIndex = 3
-            }) {
-                Text("Xml")
-            }
-        }
-        when (sampleIndex) {
-            0 -> CompositionLocalProvider(LocalKamelConfig provides kamelConfig) {
-                Gallery()
-            }
+            when (sampleIndex) {
+                0 -> Gallery()
+                1 -> FileSample("Compose.png")
+                2 -> FileSample("ComposeXml.xml")
+                3 -> FileSample("Kotlin.svg")
 
-            1 -> FileSample("Compose.png", kamelConfig, context)
-            2 -> FileSample("ComposeXml.xml", kamelConfig, context)
-            3 -> FileSample("Kotlin.svg", kamelConfig, context)
-
-            else -> Text("Invalid Sample Index")
+                else -> Text("Invalid Sample Index")
+            }
         }
     }
 }
