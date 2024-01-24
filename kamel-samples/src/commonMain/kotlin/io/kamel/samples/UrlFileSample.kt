e153dd02369d06fa96c4821ad6549bfaf4dd4bea
Luca Spinazzola <mspinluca@gmail.com>
2024-01-24 00:57:21 +0000

    add UrlFileSample for jvm/native targets
    others todo. Should use a file picker

---
+++ kamel-samples/src/commonMain/kotlin/io/kamel/samples/UrlFileSample.kt
@@ -0,0 +1,23 @@
+package io.kamel.samples
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+import okio.Path.Companion.toPath
+
+
+@Composable
+internal fun UrlFileSample(resourceFileName: String) {
+    val absolutePath = fileSystem.canonicalize("./src/commonMain/resources/$resourceFileName".toPath()).toString()
+
+    Column {
+        val painterResource = asyncPainterResource(absolutePath)
+        KamelImage(painterResource,
+            contentDescription = resourceFileName,
+            modifier = Modifier.fillMaxSize(),
+            onFailure = { throw it })
+    }
+}
