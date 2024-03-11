572085503aa3d8fff15ab23a6bd8e54c3e272d69
Luca Spinazzola <mspinluca@gmail.com>
2024-03-11 02:31:29 -0400

    implement filterIsElement on wasmJs

--- kamel-decoder/kamel-decoder-image-vector/src/wasmJsMain/kotlin/filterIsElement.kt
+++ kamel-decoder/kamel-decoder-image-vector/src/wasmJsMain/kotlin/filterIsElement.kt
@@ -1,15 +1,4 @@
-import kotlinx.dom.isElement
 import nl.adaptivity.xmlutil.dom.Element
 import nl.adaptivity.xmlutil.dom.Node
 
-@Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
-internal actual fun Sequence<Node>.filterIsElement(): Sequence<Element> =
-    mapNotNull {
-        null
-//        val node = it as org.w3c.dom.Node
-//        if (node.isElement) {
-//            node as Element?
-//        } else {
-//            null
-//        }
-    }
+internal actual fun Sequence<Node>.filterIsElement(): Sequence<Element> = filterIsInstance<Element>()
