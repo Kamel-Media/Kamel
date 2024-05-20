669c789026864bde1bdeddaf64e6106cbe83129b
Luca Spinazzola <mspinluca@gmail.com>
2024-05-19 22:54:03 -0400

    fix resources path

--- kamel-samples/src/desktopMain/kotlin/io/kamel/samples/getResourceFile.kt
+++ kamel-samples/src/desktopMain/kotlin/io/kamel/samples/getResourceFile.kt
@@ -6,7 +6,8 @@ import java.io.FileOutputStream
 
 public actual suspend fun getResourceFile(fileResourcePath: String): File {
     val file = java.io.File.createTempFile("temp", ".${fileResourcePath.substringAfterLast(".")}")
-    val ins = Thread.currentThread().contextClassLoader.getResource(fileResourcePath).openStream()
+    val fullResourcePath = "composeResources/media.kamel.kamel_samples.generated.resources/" + fileResourcePath
+    val ins = Thread.currentThread().contextClassLoader.getResource(fullResourcePath).openStream()
     FileOutputStream(file).use { os ->
         val buffer = ByteArray(4096)
         var length: Int
