aee0e6b35859ae40711505810250effeada1e4a0
Luca Spinazzola <mspinluca@gmail.com>
2025-09-01 15:18:29 -0400

    compose 1.9.0-rc01

--- kamel-samples/src/jsMain/kotlin/main.js.kt
+++ kamel-samples/src/jsMain/kotlin/main.js.kt
@@ -1,12 +1,12 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.window.CanvasBasedWindow
+import androidx.compose.ui.window.ComposeViewport
 import io.kamel.samples.launcher
 import org.jetbrains.skiko.wasm.onWasmReady
 
 @OptIn(ExperimentalComposeUiApi::class)
 public fun main() {
     onWasmReady {
-        CanvasBasedWindow("Kamel Sample: js") {
+        ComposeViewport("composeApp") {
             launcher()
         }
     }
