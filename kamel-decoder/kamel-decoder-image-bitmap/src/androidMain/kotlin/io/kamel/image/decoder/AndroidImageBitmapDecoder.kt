06edde4df62a6b2d6055121e6493853e7f7d7a20
Luca Spinazzola <mspinluca@gmail.com>
2026-01-27 15:47:48 -0500

    For API < 31, use AvifDecoder for AVIF images
    https://developer.android.com/about/versions/12/features#avif
    
    part of #221

--- kamel-decoder/kamel-decoder-image-bitmap/src/androidMain/kotlin/io/kamel/image/decoder/AndroidImageBitmapDecoder.kt
+++ kamel-decoder/kamel-decoder-image-bitmap/src/androidMain/kotlin/io/kamel/image/decoder/AndroidImageBitmapDecoder.kt
@@ -1,11 +1,13 @@
 package io.kamel.image.decoder
 
 import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import com.seiko.avif.AvifDecoder
+import com.seiko.avif.createPlatformBitmap
 import io.kamel.core.config.ResourceConfig
 import io.kamel.core.decoder.Decoder
-import io.ktor.util.*
 import io.ktor.utils.io.*
 import kotlin.reflect.KClass
 
@@ -17,8 +19,28 @@ internal actual val ImageBitmapDecoder = object : Decoder<ImageBitmap> {
 
     override suspend fun decode(channel: ByteReadChannel, resourceConfig: ResourceConfig): ImageBitmap {
         val bytes = channel.toByteArray()
-        val bitmap = BitmapFactory.decodeByteArray(bytes, Offset, bytes.size)
-            ?: throw IllegalArgumentException("Failed to decode ${bytes.size} bytes to a bitmap. Decoded bytes:\n${bytes.decodeToString()}\n")
+
+        val bitmap = if (Build.VERSION.SDK_INT < 31 && AvifDecoder.isAvifImage(bytes)) {
+            // For API < 31, use AvifDecoder for AVIF images since BitmapFactory doesn't support AVIF
+            try {
+                val decoder = AvifDecoder.create(bytes)
+                decoder.nextFrame()
+                val frame = decoder.getFrame()
+                val platformBitmap = frame.createPlatformBitmap()
+                frame.decodeFrame(platformBitmap)
+                platformBitmap
+            } catch (t: Throwable) {
+                throw IllegalArgumentException(
+                    "Failed to decode ${bytes.size} bytes to a bitmap. Decoded bytes:\n${bytes.decodeToString()}\n",
+                    t
+                )
+            }
+        } else {
+            // For API >= 31 or non-AVIF images, use BitmapFactory
+            BitmapFactory.decodeByteArray(bytes, Offset, bytes.size)
+                ?: throw IllegalArgumentException("Failed to decode ${bytes.size} bytes to a bitmap. Decoded bytes:\n${bytes.decodeToString()}\n")
+        }
+
         return bitmap.asImageBitmap()
     }
 
