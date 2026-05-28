257f8691a8157d95104985e3d68783623dbb66ba
Luca Spinazzola <mspinluca@gmail.com>
2026-05-28 18:54:45 -0400

    update target sdk

--- kamel-samples/android-app/build.gradle.kts
+++ kamel-samples/android-app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "io.kamel.samples"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         multiDexEnabled = true
